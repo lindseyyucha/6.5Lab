e68747b63cea311cf33ea0b96208a7eb208bc6c7
lindseyyucha <119362898+lindseyyucha@users.noreply.github.com>
2024-02-23 17:29:28 +0000

    wip

--- utilities.js
+++ utilities.js
@@ -1,4 +1,8 @@
+
 window.onload = function()
 {
-    
+    document.addEventListener('click', function())
+    {
+    const nameList= [Tifa, Aerith, Cloud, Barret, Yuffie, Red, Biggs, Zach, Sephiroth, Jessie]
+    }
 }
