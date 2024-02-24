c03a92e362c42cd769ad98f6f70af536b377cc6a
lindseyyucha <119362898+lindseyyucha@users.noreply.github.com>
2024-02-24 00:21:37 +0000

    wip

--- utilities.js
+++ utilities.js
@@ -1,8 +1,14 @@
 
 window.onload = function()
 {
-    document.addEventListener('click', function())
-    {
-    const nameList= [Tifa, Aerith, Cloud, Barret, Yuffie, Red, Biggs, Zach, Sephiroth, Jessie]
-    }
+    const nameList = ['Tifa', 'Aerith', 'Cloud', 'Barret', 'Yuffie', 'Red', 'Biggs', 'Zach', 'Sephiroth', 'Jessie'];
+    Alpine.data('randomizer', function() {
+        return {
+          nameList: nameList,
+          randomizeName() {
+            const randomIndex = Math.floor(Math.random() * this.nameList.length);
+            this.randomizedName = this.nameList[randomIndex];
+          }
+        };
+      });
 }
